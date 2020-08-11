63d49b1e27b3eff4a374985807cbdc203ba79cd6
Dakota Coppage <dakotakcoppage@me.com>
2020-08-11 22:41:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,14 @@
-lassie = Dog.new
-lassie.name = "Lassie"
- 
-lassie.name #=> "Lassie"
+lassie = Dog
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  
+  def name
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.name
+lassie.name
+
+puts lassie.name
